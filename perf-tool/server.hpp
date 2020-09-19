a9f4e7e4cab8ed521aad8c9d202ce871450e625e
Anas Mohamed <amohamed@ualberta.ca>
2020-09-19 16:44:06 -0600

    Integrated server into agent.
    Server runs on VM init and sends message to clients through agent.

--- perf-tool/server.hpp
+++ perf-tool/server.hpp
@@ -1,23 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h> 
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <iostream>
+#ifndef SERVER_H_
+#define SERVER_H_
+
 #include <string>
-#include <fstream>
-
-using namespace std;
-
-int sockfd, newsockfd;
 
 // Handle sending message to all clients.
-void sendMessageToClients(string message);
+void sendMessageToClients(std::string message);
 
 // Starts the server and sends a connection succeeded message.
 void startServer(int portNo);
 
-void shutDownServer()
+void shutDownServer();
+
+#endif /* SERVER_H_ */

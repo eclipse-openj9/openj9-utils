5f9a9ec432c876dab267e0dba6e6faae283cc33e
Vivian <vivianxwu@outlook.com>
2020-09-23 11:48:40 -0700

    fix conflicts

--- perf-tool/server.cpp
+++ perf-tool/server.cpp
@@ -48,6 +48,57 @@ void startServer(int portNo) {
     printf("Reached here.\n");
 
     // create a socket
+    // socket(int domain, int type, int protocol)#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <iostream>
+#include <fstream>
+#include <server.hpp>
+#include <perf.hpp>
+
+int sockfd, newsockfd;
+
+using namespace std;
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+    int portNo;
+    if (argc < 2) {
+        fprintf(stderr,"ERROR, no port provided\n");
+        exit(1);
+    }
+
+    portNo = atoi(argv[1]);
+    startServer(portNo);
+    
+    return 0; 
+}
+
+void sendMessageToClients(string message) {
+    const char *cstring = message.c_str();
+    send(newsockfd, cstring, strlen(cstring), 0);
+}
+
+void startServer(int portNo) {
+    socklen_t clilen;
+    char buffer[256], msg[256];
+    struct sockaddr_in serv_addr, cli_addr;
+    int n;
+
+    printf("Reached here.\n");
+
+    // create a socket
     // socket(int domain, int type, int protocol)
     sockfd =  socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) 
@@ -103,9 +154,36 @@ void startServer(int portNo) {
     bzero(buffer,256);
 
     n = read(newsockfd,buffer,255);
+
+    if (strcmp(buffer, "perf\n") == 0) {
+	sendPerfDataToClient();
+    }
+	
     if (n < 0) error("ERROR reading from socket");
     printf("%s\n", buffer);
 
+}
+
+void sendPerfDataToClient(void) {
+    pid_t pid, currPid;
+    json perfData;
+
+    // Fork new process to handle perf data retrieval
+    if ((pid = fork()) < 0) {
+	error("Error on forking process"); 
+    }
+
+    currPid = getpid();
+    if (pid == 0) {
+	 // Let child process handle getting perf data
+	 perfData = perfProcess(currPid); 
+	 std::string perfStr = perfData.dump();
+	 
+	 printf("Server has obtained perf data:\n%s\n", perfStr.c_str()); // for debugging
+	 
+	 // Relay data to client 
+	 sendMessageToClients(perfStr);
+    } // else parent continues on 
 }
 
 void shutDownServer() {

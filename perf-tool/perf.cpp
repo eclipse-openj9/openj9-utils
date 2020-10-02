461c97cee92433248798ccf8c1b59952527b94bb
Amy Hou <amyhou98@gmail.com>
2020-10-01 22:37:00 -0400

    pushing changes so that I can pull them to another computer's local branch

--- perf-tool/perf.cpp
+++ perf-tool/perf.cpp
@@ -4,6 +4,9 @@
 #include <string>
 #include <perf.hpp>
 #include <signal.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <fcntl.h>
 
 
 json perfProcess(pid_t processID, int recordTime) {
@@ -20,45 +23,100 @@ json perfProcess(pid_t processID, int recordTime) {
 		perror("mkdir");
 
 	// Change to /tmp folder to store the perf data
-	if (chdir("./tmp") == -1)
+	if (chdir("./tmp") == -1) {
 		perror("chdir");
+		_exit(1);
+	}
 	
 	pid_t pid;
+	int fd, status;
 
 	if ((pid = fork()) == -1) {
 		perror("fork");
+		_exit(1);
 	}
 
 	if (pid == 0) {
 		// Run perf record to collect process data into perf.data
-		if (execve(args[0], args, envp) == -1)
+		if (execve(args[0], args, envp) == -1) {
 			perror("execve");
+			_exit(1);
+		}
 	}
-	else {
-		sleep(recordTime);
-		kill(pid, SIGTERM);
-		
-		// Run perf script to get all data
-		char* cmdPerfScript = (char*) "perf script > perf.data.txt";
-		char* perfScriptArgs[5] = {"/usr/bin/perf", "script", ">", "perf.data.txt", NULL};
-		//if (execve(perfScriptArgs[0], perfScriptArgs, envp) == -1)
-		//	perror("execve");
+	sleep(recordTime);
+	kill(pid, SIGTERM);
+	pid = wait(&status);
+	system("perf script > perf.data.txt");
+	/*
+	FILE *fp;
+	char path[PATH_MAX];
+	char* filename = "perf.data.txt";
+        fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+
+	fp = popen("perf script", "r");
+	if (fp == NULL) {
+		perror("popen");
+	}
+
+	while (fgets(path, PATH_MAX, fp) != NULL) {
+		write(fd, path, PATH_MAX);
+	}
+
+	status = pclose(fp); */
+	// to-do: perform error check of status
 	
-		// Save into json format
-		json perfData;	
-
-		// Parse perf.data.txt using regex to get data for each process
-		// Temp dummy variables
-		perfData["pid"] = pidStr;
-		perfData["overhead"] = "dummyOverhead";
-		perfData["command"] = "dummyCommand";
-		perfData["sharedObject"] = "dummySharedObject";
-		perfData["symbol"] = "dummySymbol";
-
-		printf("Returning now\n");
-
-		return perfData;
+/*
+	// Run perf script to get all data
+	char* cmdPerfScript = (char*) "perf script > perf.data.txt";
+	char* perfScriptArgs[3] = {"/usr/bin/perf", "script", NULL};
+		
+	// Open file
+	char* filename = "perf.data.txt";
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	
+	if (fd == -1) {
+		perror("open");
+		_exit(1);
 	}
+
+	if (dup2(fd, STDOUT_FILENO) == -1) {
+		perror("dup2");
+		close(fd);
+		_exit(1);
+	}
+
+	if ((pid = fork()) == -1) {
+                perror("fork");
+                _exit(1);
+        }
+	
+	if (pid == 0) {
+		if (execve(perfScriptArgs[0], perfScriptArgs, envp) == -1) {
+			perror("execve");
+			_exit(1);
+		}
+	}
+	// Else, parent waits for child to return
+	pid = wait(&status);
+	close(fd);
+*/	
+	// Save into json format
+	json perfData;	
+
+	// Parse perf.data.txt using regex to get data for each process
+	// Temp dummy variables
+	perfData["pid"] = pidStr;
+	perfData["overhead"] = "dummyOverhead";
+	perfData["command"] = "dummyCommand";
+	perfData["sharedObject"] = "dummySharedObject";
+	perfData["symbol"] = "dummySymbol";
+	
+	printf("Returning now\n");
+	
+	//close(fd);
+
+	return perfData;
+	
 
 }
 
@@ -66,7 +124,7 @@ json perfProcess(pid_t processID, int recordTime) {
 	json j;
 	pid_t pid;
 	pid = getpid();
-	j = perfProcess(pid);
+	j = perfProcess(pid, 3);
 	std::string str = j.dump();
 
 	printf("%s\n", str.c_str());
